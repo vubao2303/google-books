963717abf296d8b73a56aef555776da9e425ba93
vubao2303 <bbbvut@gmail.com>
2021-03-01 22:25:43 -0800

    add save button

--- create-react-express/client/src/components/SavedCard/index.js
+++ create-react-express/client/src/components/SavedCard/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-function SavedCard (){
+function SavedCard (props){
   return (
-    <div> 
-      <h2>title</h2>
-      
-      <p> author </p>
-      <p> desctiptions </p>
-      <img src="#" alt="" />
-      <button type= "button"> view</button>
-      <button type= "button"> delete </button>
-    </div>
+    <div className="container">
+    <h2>{props.title}</h2>
+    <p> {props.author} </p>
+    <p> {props.synopsis} </p>
+    <img src={props.image} alt="" />
+    <a href={props.link} target="_blank" > <button type="button"> view</button> </a>
+    <button type="button" onClick={(event) => {
+      event.preventDefault();
+      props.saveBooks({ title: props.title, author: props.author, synopsis: props.synopsis, image: props.image, link: props.link});
+    }}> Delete </button>
+  </div>
   )
 }
 export default SavedCard;
