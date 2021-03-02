e67256b0335b68b5be10d75ae36f31d8f2e5f33d
Suda Nimh <rslepejian@gmail.com>
2021-03-01 20:44:05 -0800

    started building out Saved books page

--- create-react-express/client/src/pages/Saved.js
+++ create-react-express/client/src/pages/Saved.js
@@ -2,11 +2,28 @@ import React from "react";
 import SavedBooks from "../components/Saved";
 // import Result from "../components/Result";
 
-
 function Saved() {
+  const [books, setBooks] = useState([]);
+
+  function loadBooks(event) {
+    event.preventDefault();
+    // console.log("loadedbooks")
+    var searchName = event.target.value;
+    if (searchName === "") {
+      setBooks([]);
+    }
+
+    API.getBookList().then(() => {
+      API.getBooks(searchName).then((books) => {
+        setBooks(books)
+      });
+    })
+      .catch(err => console.log(err));
+  }
+  
   return (
     <div>
-        <SavedBooks />
+      <SavedBooks />
     </div>
   );
 }
