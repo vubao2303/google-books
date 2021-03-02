9d5e83e2e35da8fb1bc7aaabaf482296679895b2
vubao2303 <bbbvut@gmail.com>
2021-03-01 18:07:57 -0800

    add search function

---
+++ create-react-express/client/src/components/SearchForm/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function SearchForm (props) {
+  console.log(props)
+  return (
+    <div>
+      <form >
+        <label className="form-label">Look for your book</label>
+        <input onChange={(event) => {
+        event.preventDefault();
+        console.log("banana")
+        props.loadBooks(event)
+      }
+      } placeholder='Search for a book title!' type="text" className="form-control" id="exampleInput" aria-describedby="" />
+        <button type="submit" className="btn btn-primary">Search</button>
+      </form>
+    </div>
+  )
+}
+export default SearchForm;
+
