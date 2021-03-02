59e51a5bab3f9c9c3eb3ae247acf62ced8f2ce21
Suda Nimh <rslepejian@gmail.com>
2021-03-01 18:41:43 -0800

    started building out the backend api, fixed issues with model, copied template for controller WILL NEED TWEAKING

--- create-react-express/models/book.js
+++ create-react-express/models/book.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 const googlebook = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  image: { type: Image, required: true }
+  synopsis: {type: String},
+  image: { type: Image},
+  link: {type: String} 
 });
 
 const Book = mongoose.model("Book", googlebook);
